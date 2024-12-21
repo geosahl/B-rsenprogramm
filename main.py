6d72000d81c5c85f8466cba3b4141a28239f1589
Geosahl <GSaliger@aol.com>
2024-12-21 09:12:52 +0100

    Mein erster Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Börsenprogramm")
+print("Willkommen zum Börsenprogramm!")
+print("Bitte wählen Sie eine Option aus dem Menü:")
+print("1. Aktienkurs anzeigen")
+print("2. Aktien kaufen")
+print("3. Aktien verkaufen")
+print("4. Programm beenden")
+#Wünsche einen schönen Tag
+print("Ich wünsche Ihnen einen schönen Tag!")
